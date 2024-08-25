ad2575cae304c962238b518015cc75c4f48f5c7c
The Prince <princedumanhug@gmail.com>
2024-08-26 02:39:47 +0800

    feat: created template files

---
+++ wp-content/themes/universitytheme/footer.php
@@ -0,0 +1,5 @@
+<p>Greeting from footer</p>
+
+</body>
+
+</html>
---
+++ wp-content/themes/universitytheme/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <?php wp_head(); ?>
+ <title>Document</title>
+</head>
+
+<body>
+ <header>
+  <nav>
+   <p>Logo</p>
+   <div>
+    <ul>
+     <li>Home</li>
+     <li>About</li>
+    </ul>
+   </div>
+  </nav>
+ </header>
