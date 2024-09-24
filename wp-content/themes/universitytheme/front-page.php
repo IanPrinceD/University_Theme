dd9ec5ce1aa96384724e994c1798e50fd224787d
The Prince <princedumanhug@gmail.com>
2024-09-25 07:11:47 +0800

    feat: added content on the front-page.php

---
+++ wp-content/themes/universitytheme/front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+ <div class="page-banner__bg-banner"></div>
+ <div class="page-banner__content container container--narrow">
+  <h1 class="page-banner__title">Welcome to our University!</h1>
+  <div class="page-banner__intro">
+   <p>Explore and Learn in here.</p>
+  </div>
+ </div>
+</div>
+
+<div class="container container--narrow page-section">
+
+</div>
+
+<?php get_footer(); ?>
